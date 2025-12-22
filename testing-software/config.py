cc722b9cea98738cd4099035f6d7576538ce18da
theanywayguy <ajajiphone.am@gmail.com>
2025-12-22 18:59:00 +0300

    added aegis targeting system all files have seperate concerns 4 modes all,red,blue,cherry pick same requirements as our main software

---
+++ testing-software/config.py
@@ -0,0 +1,14 @@
+# aegis_config.py
+
+# ---------------- CONFIGURATION ----------------
+MODEL_PATH = "best.pt"
+CONF_THRESHOLD = 0.5
+LOCK_FRAME_THRESHOLD = 5 
+PRECISION_RADIUS = 15  
+DEADZONE = 2            
+
+# High-Visibility Engineering Palette
+CLR_UI_CYAN = "#fc0b0b"
+CLR_BG = "#080808"
+CLR_PANEL = "#111"
+CLASS_MAP = {0: ("dost", (0, 255, 0)), 1: ("dusman", (255, 105, 180))}
